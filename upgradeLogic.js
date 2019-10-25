28fd88583fc40af917ba46e5139db99fc835f106
zhangyong <houyhea@126.com>
2019-10-25 14:40:40 +0800

    s

--- upgradeLogic.js
+++ upgradeLogic.js
@@ -1,18 +1,13 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.upgradeLogic = upgradeLogic;
-
 /**
  * 返回是否要强制更新
- * @param {*} projPackageJson 
- * @param {*} kitPackageJson 
+ * @param {*} projPackageJson 项目
+ * @param {*} kitPackageJson 项目本地的kit
+ * @param {*} newKitPackageJson 远端新版kit
  */
-function upgradeLogic(projPackageJson, kitPackageJson) {
+function upgradeLogic(projPackageJson,kitPackageJson,newKitPackageJson){
   console.log("远端包的执行：");
   console.log(projPackageJson);
   console.log(kitPackageJson);
+  console.log(newKitPackageJson);
   return true;
 }
